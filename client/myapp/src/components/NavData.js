adf69a25176f87e1a9c78e7f2ab4ab99a8c08ec0
Bridgit Arika <bridgitmob@gmail.com>
2021-01-27 13:12:01 -0600

    utils

---
+++ client/myapp/src/components/NavData.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import HomeIcon from '@material-ui/icons/Home';
+
+
+export const NavData = [
+    {
+        title: "Home",
+        icon: <HomeIcon />,
+        link: "/home"
+    },
+    {
+        title: "Categories",
+        link: "/explore"
+    },
+    {
+        title: "Category 1", 
+        link: "/category/1"
+    },
+    {
+        title: "Category 2", 
+        link: "/category/2"
+    },
+    {
+        title: "Category 3", 
+        link: "/category/3"
+    }
+   
+   
+]
