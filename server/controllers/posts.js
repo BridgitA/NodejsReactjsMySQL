494ae68ca645699f87155cf6eafea9ba4d90a559
Bridgit Arika <bridgitmob@gmail.com>
2021-02-02 18:18:22 -0600

    finished the app

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,25 +1,41 @@
+import express from 'express';
 import mongoose from 'mongoose';
+
 import PostMessage from '../models/postMessage.js';
 
-export const getPosts = async (req, res) => {
+const router = express.Router();
+
+export const getPosts = async (req, res) => { 
     try {
         const postMessages = await PostMessage.find();
-
+                
         res.status(200).json(postMessages);
     } catch (error) {
-        res.status(404).json({message: error.message });
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getPost = async (req, res) => { 
+    const { id } = req.params;
+
+    try {
+        const post = await PostMessage.findById(id);
+        
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
 }
 
 export const createPost = async (req, res) => {
     const post = req.body;
 
-    const newPOst = new PostMessage(post);
+    const newPostMessage = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() })
 
     try {
-        await newPOst.save();
-        
-        res.status(201).json(newPost);
+        await newPostMessage.save();
+
+        res.status(201).json(newPostMessage );
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
@@ -51,11 +67,24 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
     const { id } = req.params;
 
+    if (!req.userId) {
+        return res.json({ message: "Unauthenticated" });
+      }
+
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
     
     const post = await PostMessage.findById(id);
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
-    
-    res.json(updatedPost);
+    const index = post.likes.findIndex((id) => id ===String(req.userId));
+
+    if (index === -1) {
+      post.likes.push(req.userId);
+    } else {
+      post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+    res.status(200).json(updatedPost);
 }
+
+
+export default router;
